b76c26f651415bf023abc2c40c7bc42b948b1cae
Carl Timm <ctiv@hotmail.com>
2014-07-07 14:50:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BalanceCode::Application.config.secret_token = 'dc3355f0af0c9f3e0be16c3cd31dc09f793d39e60b5c84968ec77ba6965c0324f40335b4cd658c9d75d99ac383bb7a62cfe38f42fe605eb1495aed668e4db694'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BalanceCode::Application.config.session_store :cookie_store, key: '_Balance_Code_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BalanceCode::Application.config.session_store :active_record_store
